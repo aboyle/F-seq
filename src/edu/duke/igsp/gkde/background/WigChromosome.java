cd3df29b088a66d57b9d8a9618f84ada7bbf0fb3
Alan Boyle <aboyle@gmail.com>
2013-02-15 13:21:22 -0800

    Update src/edu/duke/igsp/gkde/background/WigChromosome.java

--- src/edu/duke/igsp/gkde/background/WigChromosome.java
+++ src/edu/duke/igsp/gkde/background/WigChromosome.java
@@ -1,3 +1,30 @@
+/*****************************************************************************
+  WigChromosome.java
+
+  (c) 2008-2013 - Alan Boyle
+  Department of Genetics
+  Stanford University
+  aboyle@gmail.com
+
+  Licensed under the GNU General Public License 3.0 license.
+
+  This file is part of F-seq.
+
+  F-seq is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  F-seq is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with F-seq.  If not, see <http://www.gnu.org/licenses/>.
+
+******************************************************************************/
+
 package edu.duke.igsp.gkde.background;
 public class WigChromosome {
 		 
